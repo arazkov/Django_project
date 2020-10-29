a0d11ed74c5de97adef7d7db8d288f4e6cc4dbbe
ara <arazkov@yandex.ru>
2020-10-29 23:35:18 +0300

    info time from Weather app V0.2.2

--- weather/views.py
+++ weather/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+import datetime as dt
 
 def weather_def(request):
-    return HttpResponse('Weather page')
+    time_now = dt.datetime.now().strftime('%H:%M')
+    return HttpResponse('Weather page\ntime now {}'.format(time_now))
 
