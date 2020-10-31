289723e4e8d5ede68ecb0dee665df2d369cc5456
ara <arazkov@yandex.ru>
2020-10-31 21:31:17 +0300

    add view weather from ip in Weather app V0.2.3

--- weather/views.py
+++ weather/views.py
@@ -1,8 +1,32 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 import datetime as dt
+from requests import get
+import requests, json
+
+def what_city(ip):
+    result = json.loads(get(f'http://ip-api.com/json/{ip}?lang=ru').text)
+    return result['city']
+
+def what_weather(city):
+    url = f'http://wttr.in/{city}'
+    weather_parameters = {
+        'format': 3,
+        'M': ''
+    }
+    try:
+        response = requests.get(url, params=weather_parameters)
+    except requests.ConnectionError:
+        return '<сетевая ошибка>'
+    if response.status_code == 200:
+        return response.text.strip()
+    else:
+        return '<ошибка на сервере погоды. попробуйте позже>'
 
 def weather_def(request):
     time_now = dt.datetime.now().strftime('%H:%M')
-    return HttpResponse('Weather page\ntime now {}'.format(time_now))
+    ip = get('https://api.ipify.org').text
+    return HttpResponse('Weather page,\n  '
+                        'time now {}, My public IP address is: {}, '
+                        'weather {}'.format(time_now, ip, what_weather(what_city(ip))))
 
