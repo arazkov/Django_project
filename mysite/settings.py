add993ed67b96669037ea7630e55cd6b91699849
ara <arazkov@yandex.ru>
2020-11-01 10:13:50 +0300

    fix bag from security V0.2.4

--- mysite/settings.py
+++ mysite/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '3qlpk_!9y5*e(madfh4a!ae$sm*+n+w4v4@_r$p_&-li-95do='
+SECRET_KEY = ''
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
