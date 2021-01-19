adf7bd33e16989b52517cd30d820bc15cc45568a
arazkov <arazkov@yandex.ru>
2021-01-19 17:04:37 +0300

    V 0.3.1 edit size image from fantlab app

--- fantlab/views.py
+++ fantlab/views.py
@@ -6,11 +6,16 @@ import requests, json
 def fantlab_index(request):
     base = json.loads(get('https://api.fantlab.ru/user/50656/marks?page=1&sort=date&type=novel').text)
     works_list = base['items']
-    work_id = base['items'][0]['work_id']
-    work_base = json.loads(get(f'https://api.fantlab.ru/work/{work_id}').text)
-    work_description = work_base['work_description']
-    #print(autor_name)
+    anotation = []
+    for work in works_list:
+        work_id = work['work_id']
+        work_base = json.loads(get(f'https://api.fantlab.ru/work/{work_id}').text)
+        work_description = work_base['work_description']
+        anotation.append(work_description)
+    
+    
+    
     return render(request, 'fantlab/fantlab_list.html', {
-        'ano' : work_description[:150],
+        'ano' : anotation[1],
         'list' : works_list,
         })
