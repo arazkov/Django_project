59cfd53dd82c44384770fcf38f9e2bd529499319
ara <arazkov@yandex.ru>
2021-01-19 23:07:34 +0300

    V 0.3.2 criate to view anotation in fantlab app

--- fantlab/views.py
+++ fantlab/views.py
@@ -6,16 +6,14 @@ import requests, json
 def fantlab_index(request):
     base = json.loads(get('https://api.fantlab.ru/user/50656/marks?page=1&sort=date&type=novel').text)
     works_list = base['items']
-    anotation = []
     for work in works_list:
         work_id = work['work_id']
         work_base = json.loads(get(f'https://api.fantlab.ru/work/{work_id}').text)
         work_description = work_base['work_description']
-        anotation.append(work_description)
-    
+        work['anotation'] = work_description
+            
     
     
     return render(request, 'fantlab/fantlab_list.html', {
-        'ano' : anotation[1],
         'list' : works_list,
         })
