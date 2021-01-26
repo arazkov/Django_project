d8983b33be1bf71fe6162e8693b8611df6c1d0f1
ara <arazkov@yandex.ru>
2021-01-26 21:16:41 +0300

    v 0.3.3 do to views 20 books in fantlab app

--- fantlab/views.py
+++ fantlab/views.py
@@ -1,19 +1,17 @@
 from django.shortcuts import render
 from requests import get
-import requests, json
+import json
 
 
 def fantlab_index(request):
     base = json.loads(get('https://api.fantlab.ru/user/50656/marks?page=1&sort=date&type=novel').text)
     works_list = base['items']
-    for work in works_list:
+    for work in works_list[:20]:
         work_id = work['work_id']
         work_base = json.loads(get(f'https://api.fantlab.ru/work/{work_id}').text)
         work_description = work_base['work_description']
         work['anotation'] = work_description
-            
-    
-    
+
     return render(request, 'fantlab/fantlab_list.html', {
-        'list' : works_list,
-        })
+        'list': works_list,
+    })
