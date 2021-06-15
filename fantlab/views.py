997ed53a920755620e10e17c92ff16f249d54cde
Vladimir_pc <arazkov@ya.ru>
2021-06-15 23:00:35 +0300

    v 0.3.5 add pagination to fantlab app and add json file to test app faster

--- fantlab/views.py
+++ fantlab/views.py
@@ -1,17 +1,30 @@
 from django.shortcuts import render
 from requests import get
 import json
+from django.core.paginator import Paginator
 
+
+# def fantlab_index(request):
+#     base = json.loads(get('https://api.fantlab.ru/user/50656/marks?page=1&sort=date&type=novel').text)
+#     works_list = base['items']
+#     for work in works_list:
+#         work_id = work['work_id']
+#         work_base = json.loads(get(f'https://api.fantlab.ru/work/{work_id}').text)
+#         work_description = work_base['work_description']
+#         work['anotation'] = work_description
+#     paginator = Paginator(works_list, 10)
+#     page_number = request.GET.get('page')
+#     page_obj = paginator.get_page(page_number)
+#     return render(request, 'fantlab/fantlab_list.html', {
+#         'list': page_obj,
+#     })
 
 def fantlab_index(request):
-    base = json.loads(get('https://api.fantlab.ru/user/50656/marks?page=1&sort=date&type=novel').text)
-    works_list = base['items']
-    for work in works_list[:20]:
-        work_id = work['work_id']
-        work_base = json.loads(get(f'https://api.fantlab.ru/work/{work_id}').text)
-        work_description = work_base['work_description']
-        work['anotation'] = work_description
-
+    with open('/home/ara/python/Django_project/fantlab/data.json', 'r') as js:
+        json_file = json.load(js)
+    paginator = Paginator(json_file['items'], 20)
+    page_number = request.GET.get('page')
+    page_obj = paginator.get_page(page_number)
     return render(request, 'fantlab/fantlab_list.html', {
-        'list': works_list,
+        'list': page_obj,
     })
